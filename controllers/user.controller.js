c25b28dbe185822aa8df896d7edd6901eaa13749
Neha Patange <nehapatange2004@gmail.com>
2025-09-07 11:59:35 +0530

    added profilePic upload via cloudinary with payload limit as 5MB

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,15 @@
 import User from "../models/User.js";
 import { error } from "../utils/error.utils.js";
+import dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
+dotenv.config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_secret: process.env.API_SECRET,
+    api_key: process.env.API_KEY,
+});
+
 
 export const getAllUsers = async (req, res) => {
     try {
@@ -16,9 +26,14 @@ export const getUserProfile = async (req, res) => {
     try {
         const userId = req.params.userId;
         const user = await User.findById(userId).lean();
+
         delete user.password;
-        delete user.connectionRequests;
-        delete user.savedJobs;
+
+        if (userId.toString() !== req.user._id.toString()) {
+            delete user.connectionRequests;
+            delete user.savedJobs;
+            return res.status(200).send(user);
+        }
 
         return res.status(200).send(user);
     } catch (err) {
@@ -40,13 +55,22 @@ export const updateUserProfile = async (req, res) => {
         const allowedFields = [
             "name",
             "bio",
-            "profilePic",
             "experience",
             "education",
             "skills",
         ];
 
         const filteredUpdates = {};
+
+        if (req.body.profilePic) {
+            const response = await cloudinary.uploader.upload(req.body.profilePic, {
+                public_id: `profilePic-${userId}`,
+                overwrite: true,
+                folder: "samples/test1",
+            });
+            filteredUpdates["profilePic"] = response.secure_url
+        }
+
         for (const key of allowedFields) {
             if (updates[key] !== undefined) {
                 filteredUpdates[key] = updates[key];
--- index.js
+++ index.js
@@ -34,7 +34,8 @@ app.use(
     credentials: true,
   })
 );
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
 app.use(cookieParser());
 
 // Routes
